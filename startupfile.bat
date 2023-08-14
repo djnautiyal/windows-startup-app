aa257fe33b9fdac4397a8ab2eb0c7f9b765c722e
djnautiyal <dhannanjainautiyal@gmail.com>
2023-08-14 23:23:33 +0530

    Closing the cmd after opening msedge

--- startupfile.bat
+++ startupfile.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" file:///C:/Users/dhann/Documents/windows-startup-app/life-chart.html
+start iexplore.exe file:///C:/Users/dhann/Documents/windows-startup-app/life-chart.html
 exit
